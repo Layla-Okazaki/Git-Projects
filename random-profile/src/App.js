020bec3daf52d6f037eea530f3973052115c9be4
Layla Okazaki <laylaandart@gmail.com>
2023-01-10 15:39:01 +0900

    comments from Mikey

--- random-profile/src/App.js
+++ random-profile/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+// importing file (for styling)
 import './App.css';
+// Importing the values/functions from react (in node modules)
+import { useState } from 'react';
 
-function App() {
+// The only thing we're exporting from this file (A function in this case)
+// You can import this from other files and access the value of it
+// in this case the App function as imported in index.js
+export default function App() {
+  const [count, setCount] = useState(); // value of useState() is [variable, function]
+  // useState is only used when declaring the variable
+  // use the variable to access the value use the function to change the value
+  // without useState the website won't refresh and the updated values won't appear
+  // example of useState on line 19
+
+  // handleClick called at line 31 when user clicks button
+  function handleClick() {
+    // function to change the value of count (function already defined at line 10)
+    setCount((prevCount) => {
+      // prevCount is the previous value of count
+      // what you return in the function will be the new value of count
+      return prevCount + 1;
+    });
+  }
+
+  // the code that's like HTML below is called JSX and react compiles it to dom looking data
+  // you can use {} to write javascript in the JSX
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{count}</h1>
+      <button onClick={handleClick}>increase count</button>
     </div>
   );
 }
-
-export default App;
--- random-profile/src/index.js
+++ random-profile/src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// imports the export default from ./App.js
+// the value of App is what you exported as default in ./App.js
 import App from './App';
+// imports the webVitals explained on line 14, 15
 import reportWebVitals from './reportWebVitals';
 
+// make the div with the root id in public/index.html to the react root witch is where the <App /> will be rendered
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reacts custom error messages that show on the website instead of the console
+// only made to use in development state
 reportWebVitals();
